0b8659dc944198ed2c9a217fa38df937ba35fbb7
janegl2 <78577524+janegl2@users.noreply.github.com>
2021-11-09 20:56:48 -0500

    :''(

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -23,6 +23,7 @@ class Grupo:
 
     def agregarAlumno(self, alumno, lista=None):
         if(lista is None):
+            lista = []
             lista.append(alumno)
             self.listadoAlumnos = self.listadoAlumnos + lista
         else:
